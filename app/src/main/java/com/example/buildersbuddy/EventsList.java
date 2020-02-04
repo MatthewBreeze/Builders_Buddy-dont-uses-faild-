e0907484e79eec33765a4b77e0d596a8fc304259
MatthewBreeze <43578023+MatthewBreeze@users.noreply.github.com>
2020-02-04 15:27:29 +0000

    fxing git isuess

--- app/src/main/java/com/example/buildersbuddy/EventsList.java
+++ app/src/main/java/com/example/buildersbuddy/EventsList.java
@@ -2,73 +2,34 @@ package com.example.buildersbuddy;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.MenuItemCompat;
 
-<<<<<<< HEAD:app/src/main/java/com/example/buildersbuddy/Events/EventsList.java
-import com.example.buildersbuddy.Home_Page;
-import com.example.buildersbuddy.R;
-import com.example.buildersbuddy.TradsCard.TradeCards;
-import com.google.firebase.FirebaseApp;
-=======
->>>>>>> parent of c0006e8... Layout and pages. code needs implemeting:app/src/main/java/com/example/buildersbuddy/EventsList.java
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class EventsList extends AppCompatActivity {
 
-
     FirebaseAuth Auth;
     DatabaseReference mDatabase;
     ListView ListView;
-    Events events;
-
-    private ArrayList<String> arrayList = new ArrayList<>();
-    private ArrayAdapter<String> adapter;
+    ArrayList<String>  List;
+    ArrayAdapter<String> adapter;
+    Events cEevents;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_list);
-<<<<<<< HEAD:app/src/main/java/com/example/buildersbuddy/Events/EventsList.java
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        events = new Events();
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Events");
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
-        ListView = (ListView) findViewById(R.id.ListView);
-        ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
-                startActivity(new Intent(getApplicationContext(), Event.class)
-                        .putExtra("date",events.getDate())
-                        .putExtra("location",events.getLocation())
-                        .putExtra("time", events.getTime())
-                        .putExtra("event",events.getEvent()));
-
-            }
-        });
-
-
-        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
-=======
         Auth = FirebaseAuth.getInstance();
         if(Auth.getCurrentUser()== null)
         {
@@ -81,21 +42,15 @@ public class EventsList extends AppCompatActivity {
         adapter = new ArrayAdapter<String>(this,R.layout.event_info,R.id.ListView, List);
         ListView.setAdapter(adapter);
         mDatabase.addValueEventListener(new ValueEventListener() {
->>>>>>> parent of c0006e8... Layout and pages. code needs implemeting:app/src/main/java/com/example/buildersbuddy/EventsList.java
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot)
             {
-                for(DataSnapshot ds: dataSnapshot.getChildren())
-                {
+                for(DataSnapshot ds: dataSnapshot.getChildren()) {
 
-                    events = ds.getValue(Events.class);
-                    //if(events.getId() == Auth.getUid())
-                   // {
-                        arrayList.add(events.getDate() + "     |     "+ events.getLocation()+"     |      "+events.getEvent());
-                  //  }
+                    cEevents = ds.getValue(Events.class);
+                    List.add(cEevents.getDate()+cEevents.getLocation()+cEevents.getTime()+cEevents.getDate());
                 }
                 ListView.setAdapter(adapter);
-
             }
 
             @Override
@@ -104,12 +59,7 @@ public class EventsList extends AppCompatActivity {
 
             }
         });
-    }
 
-    public void Back(View view)
-    {
-        startActivity(new Intent(getApplicationContext(), Calender.class));
-        finish();
     }
-
 }
+
--- app/src/main/java/com/example/buildersbuddy/Home_Page.java
+++ app/src/main/java/com/example/buildersbuddy/Home_Page.java
@@ -7,9 +7,12 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import com.example.buildersbuddy.Events.Calender;
 import com.example.buildersbuddy.TradsCard.TradeCards;
 import com.example.buildersbuddy.tax.TaxPage;
+=======
+>>>>>>> parent of c0006e8... Layout and pages. code needs implemeting
 =======
 >>>>>>> parent of c0006e8... Layout and pages. code needs implemeting
 import com.google.firebase.auth.FirebaseAuth;
